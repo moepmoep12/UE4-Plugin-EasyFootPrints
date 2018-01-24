27faade5ee00d3f094a9033634e2d1e674f217c8
Andi <moepmoep12@gmail.com>
2018-01-24 22:25:42 +0100

    Changed folder structure. Created custom physmaterial.

---
+++ SnowTesting/Plugins/EasyFootPrints/Source/EasyFootPrints/Public/PhysMaterial_EasyFootPrints.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PhysicalMaterials/PhysicalMaterial.h"
+#include "Sound/SoundCue.h"
+#include "PhysMaterial_EasyFootPrints.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EASYFOOTPRINTS_API UPhysMaterial_EasyFootPrints : public UPhysicalMaterial
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = EasyFootPrints)
+		UParticleSystem* ParticleSystem;
+	UPROPERTY()
+		USoundCue* FootstepSound;
+	
+};
